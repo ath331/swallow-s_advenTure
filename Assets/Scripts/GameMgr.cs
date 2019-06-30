4693c24ad7013e4889b3c47397668ac2fd96fa5f
AnTaeHwan <kstnftk@naver.com>
2019-07-01 03:34:40 +0900

    기초게임매니저 스크립트 제작

---
+++ Assets/Scripts/GameMgr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public static GameMgr instance = null;
+    public GameObject player;
+
+    public Transform[] floors;//Stage의 Floor들을 배열로 받기위한 변수
+    public Transform[] skills;//skill들을 저장하기 위한 변수
+
+
+    private void Awake()
+    {
+        instance = this;
+        Debug.Log("게임매니저 스크립트을 싱글턴화");
+    }
+
+    void Start()
+    {
+        floors = GameObject.Find("Stage").GetComponentsInChildren<Transform>();
+        skills = GameObject.Find("SkillPanel").GetComponentsInChildren<Transform>();
+        player.transform.position = floors[5].transform.position; //Player위치를 배열5번칸 (2행1열)로 배치
+        Debug.Log("Plater위치 할당");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SkillsMgr.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsMgr : MonoBehaviour
+{
+    //##############스킬들을의 정보를 저장하는 스크립트(나중에 디비로 옮겨서 관리할 생각중)
+    [HideInInspector]
+    public string name;
+    [HideInInspector]
+    public string skillName;
+
+}
